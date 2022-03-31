11be457804874373c6982987d9fe52659ed6f39e
--get <67471674+ninebolt6@users.noreply.github.com>
2022-03-31 14:01:06 +0900

    :+1: 例外への対応強化

--- util.py
+++ util.py
@@ -1,11 +1,16 @@
+from datetime import datetime
 import unicodedata
 import re
 
 def extract_number(str):
   regex = ".*?(\d+)"
-  return re.compile(regex).match(str).group(1)
+  result = re.compile(regex).match(str)
+  return "-" if result is None else result.group(1)
 
 def to_hankaku(str):
   return unicodedata.normalize("NFKC", str)
+
+def format_date(time: datetime):
+  return "{0:%Y年%m月%d日 %H時%M分%S秒}".format(time)
 
 # TODO: pytestでテスト書きたい
